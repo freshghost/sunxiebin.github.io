454f7cf9ccf33e32b2ae874778ce310a4245df2b
sunxiebin.github.io <freshghost1234@icloud.com>
2020-08-29 18:07:17 +0800

    update somethings

--- AI/NLP/python_nlp/main.py
+++ AI/NLP/python_nlp/main.py
@@ -1,7 +1,28 @@
 import nltk
+from nltk.corpus import treebank
 # nltk.download()
-sentence = "......."
+sentence = "sunxiebin is a good man"
 tokens = nltk.word_tokenize(sentence)
 print(tokens)
 tagged = nltk.pos_tag(tokens)
 print(tagged)
+
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+print(t)
+t.draw()
+
+#from nltk.chat import chatbots
+
+#chatbots()
+
+# import sys,os
+# import nltk
+# from nltk.corpus import treebank
+# from nltk.tree import Tree
+
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+# sentTree ="(IP (NP(NR 张三)) (VP(VV 参加) (AS 了) (NP (NN 会议))))"
+
+# tree= Tree.fromstring(sentTree)
+# tree.draw
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 This is a paragraph.
 
 
+
+
+
+
